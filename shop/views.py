073fefd6101473710ca4ae365f7e8ea957c536a9
Sreeparvathy770 <111829890+Sreeparvathy770@users.noreply.github.com>
2023-07-12 09:47:29 +0530

    Add files via upload

---
+++ shop/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello World")
+# Create your views here.
+def about(request):
+    return HttpResponse("This is about us page")
+def contact(request):
+    return HttpResponse("This is Contact page")
